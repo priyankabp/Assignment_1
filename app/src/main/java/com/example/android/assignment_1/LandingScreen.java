e18c9c5b9f69906ece0a6fd867db5bc128df676e
priyankabp <pbphapale@gmail.com>
2017-02-08 15:13:56 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/assignment_1/LandingScreen.java
@@ -0,0 +1,17 @@
+package com.example.android.assignment_1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Bhushan on 2/8/2017.
+ */
+
+public class LandingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_screen);
+    }
+}
