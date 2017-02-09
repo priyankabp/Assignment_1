86231a17e9dd81aa69a3c6644d4e0028e0415e08
priyankabp <pbphapale@gmail.com>
2017-02-09 08:34:33 -0500

    Create an activity  "forgotPassword" with one textview,one edittext for username,one edittext for email,one button .

--- app/src/main/java/com/example/android/assignment_1/ForgotPasswordPage.java
+++ app/src/main/java/com/example/android/assignment_1/ForgotPasswordPage.java
@@ -3,13 +3,11 @@ package com.example.android.assignment_1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ForgotPasswordPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_forgot_password_page);
     }
-
 }
