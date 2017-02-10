4c3055862754be5d02cd31d02575f8ac23f1de2c
priyankabp <pbphapale@gmail.com>
2017-02-09 20:14:31 -0500

    Add a new EditText (DoB field) on the Registration Page for Date of Birth.When clicked on it,it shows DatePickerDialog, accepts selected date, and displays it on the DoB field.

--- app/src/main/java/com/example/android/assignment_1/RegistrationPage.java
+++ app/src/main/java/com/example/android/assignment_1/RegistrationPage.java
@@ -1,13 +1,59 @@
 package com.example.android.assignment_1;
 
+import java.util.Calendar;
+
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 
 public class RegistrationPage extends AppCompatActivity {
 
+    Calendar calendar = Calendar.getInstance();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_page);
+
+        EditText datetext = (EditText) findViewById(R.id.Registration_DoB);
+        datetext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(RegistrationPage.this, date, calendar
+                        .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
     }
+
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, monthOfYear);
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+
+    };
+
+    private void updateLabel() {
+
+        String myFormat = "MM/dd/yy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(myFormat, Locale.US);
+        EditText datetext = (EditText) findViewById(R.id.Registration_DoB);
+        datetext.setText(simpleDateFormat.format(calendar.getTime()));
+    }
+
+
 }
